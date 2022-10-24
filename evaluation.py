37f8171f268876c36ec99927c2be72b818a28e33
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2022-10-24 20:13:44 +0800

    rename

--- evaluation.py
+++ evaluation.py
@@ -1,58 +1,53 @@
 import os
-import pandas as pd
 import json
-from evaluationTool import *
+from EvaTool import *
 
-def precision_recall():
-    print("calculate precision recall")
-    book_user_test = pd.read_csv(os.path.join("data", "book", "cate3_test.csv"))
-    print("test book user Ok")
-
-    recommend_list = {}
-
-    with open(os.path.join("result", "CF", "recommend_list.json"),"r") as jf:
-        recommend_list = json.load(jf)
-    eva = Evaluator(
-        recommend_list=recommend_list, testing_ans=book_user_test
-    )
-    cmp_diff_n = eva.different_topN(max_topN=1000, rule_table=None)
-    with open(os.path.join("result", "CF","metrics","metrics.json"), "w+") as log:
-        json.dump(cmp_diff_n, log, indent=4, ensure_ascii=False)
-
-def vis_topn():
-    print("vis metrics")
-    cmp_diff_n = {}
-    with open(os.path.join("result", "CF","metrics","metrics.json"), "r") as log:
-        cmp_diff_n = json.load(log)
-    
+def plotmetrics(cmp_diff_n:dict, savepath)->None:
+    print("Precision_Recall_F1 ", end="")
     VisualizationKit.plot_cmp_different_n(
-        prec=cmp_diff_n['precision'],
-        recall=cmp_diff_n['recall'],
-        f1=cmp_diff_n['f1']['val'],
-        f1maxpos=cmp_diff_n['f1']['maxpos'],
+        prec=cmp_diff_n['precision'],recall=cmp_diff_n['recall'],
+        f1=cmp_diff_n['f1']['val'],f1maxpos=cmp_diff_n['f1']['maxpos'],
         max_topN=1000,
-        savepath=os.path.join("result","CF", "metrics","metrics.jpg"),
+        savepath=savepath,
         showinline=False    
     )
-    print("PrecisionRecallF1 ", end="")
-    print(os.path.join("result","CF", "metrics","metrics.jpg"))
+    print(savepath)
+
+def plotPR(cmp_diff_n:dict, savepath):
+    print("PR ", end="")
     VisualizationKit.PR_curve(
         precision=cmp_diff_n['precision'], recall=cmp_diff_n['recall'],
-        savepath=os.path.join("result","CF","metrics","PR.jpg")
+        savepath=savepath, showinline=False
     )
-    print("PR ", end="")
-    print(os.path.join("result","CF","metrics","PR.jpg"))
+    
+    print(savepath)
 
+
+def plotROC( cmp_diff_n:dict, savepath):
+    print("ROC ", end="")
     VisualizationKit.ROC(
         cmp_diff_n['fpr'], cmp_diff_n['recall'],
-        savepath=os.path.join("result","CF", "metrics","ROC.jpg")
+        savepath=savepath, showinline=False
     )
-    print("ROC ", end="")
-    print(os.path.join("result","CF", "metrics","ROC.jpg"))
+    print(savepath)
+
+
+def main():
+    cmp_diff_n = {}
+    if not os.path.exists(os.path.join("result", "CF","metrics","metrics.json")):
+        cmp_diff_n = precision_recall(
+            predictionfile=os.path.join("result", "CF", "recommend_list.json"),
+            gthcsv=os.path.join("data", "book", "cate3_test.csv"),
+            topN_range=1000,
+            savepath=os.path.join("result", "CF","metrics","metrics.json")
+        )    
+    else:
+        with open(os.path.join("result", "CF","metrics","metrics.json"), "r") as log:
+            cmp_diff_n = json.load(log)
+    
+    #plotmetrics(cmp_diff_n, os.path.join("result","CF", "metrics","metrics.jpg"))
+    #plotPR(cmp_diff_n,os.path.join("result","CF","metrics","PR.jpg"))
+    plotROC(cmp_diff_n, os.path.join("result","CF", "metrics","ROC.jpg"))
 
 if __name__ == "__main__":
-    
-    if not os.path.exists(os.path.join("result", "CF","metrics","metrics.json")):
-        precision_recall()    
-    
-    vis_topn()
+    main()
