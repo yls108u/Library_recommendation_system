e817035adfe4936db923cb85400411e112352755
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2022-12-13 08:12:55 +0800

    adding comparison plot

--- RS/utils/plotutils.py
+++ RS/utils/plotutils.py
@@ -22,32 +22,32 @@ def plot_tsne_2d(X,name,savepath, labels=None, tsneresult=False, showinline=True
     if tsneresult:
         return xtsne
 
-def plot_PRF1_different_n(prec, recall, f1, savepath, showinline=True):
+def plot_PRF1_different_n(prec, recall, f1, savepath, showinline=True, annotate=True):
         
     max_topN = len(prec)
     f1maxpos = int(np.argmax(np.array(f1)))
     # f1_max = f1maxpos-1 because starting from N = 0
-    x_axis = list(k for k in range(0,max_topN))
+    x_axis = list(k for k in range(1,max_topN+1))
     plt.figure(dpi=800)
     plt.plot(x_axis, prec, label = "precision",color="orange")
     plt.plot(x_axis,recall,label = "recall",color = "blue")
     plt.plot(x_axis,f1,label = "f1",color="green")
     plt.scatter(f1maxpos, f1[f1maxpos], color="purple")
-    
-    plt.annotate(
+    if annotate:
+        plt.annotate(
         text=f"Top N : {f1maxpos-1}\nF1 max : {f1[f1maxpos]:.2f}\nPrecision : {prec[f1maxpos]:.2f}\nRecall : {recall[f1maxpos]:.2f}", 
-        xytext=(f1maxpos+len(f1)*0.3, f1[f1maxpos]+0.05),color="black",
+        xytext=(f1maxpos+len(f1)*0.3, f1[f1maxpos]+0.1),color="black",
         xy=(f1maxpos, f1[f1maxpos]),
         arrowprops={
             'width':0.01,'headlength':10,'headwidth':4,
             'facecolor':"black","shrink":0
         },
         va = "center",
-        bbox = dict(boxstyle="square", fc="white"),fontsize=12
+        bbox = dict(boxstyle="square", fc="white"),fontsize=9
     )
         
     plt.xlabel("N")
-    plt.title("Top N")
+    plt.title(f"Top N: ~ {max_topN}")
     plt.legend()
     plt.tight_layout()
     plt.savefig(savepath)
@@ -89,6 +89,17 @@ def plotLoss(loss:list, savename:os.PathLike,showinline=True, require=min)->None
     best_v = require(loss)
     plt.figure(dpi=800)
     plt.plot(epoch, loss, label = f"best: {best_v}")
+    plt.legend()
+    plt.savefig(savename)
+    if not showinline:
+        plt.close()
+
+def plot_comparison(x:list, labels:list, title:str, savename:os.PathLike, showinline=True):
+    e = list(_ for _ in range(1,len(x[0])+1))
+    plt.figure(dpi=800)
+    for xi, label in zip(x, labels):
+        plt.plot(e, xi, label = label)
+    plt.title(title)
     plt.legend()
     plt.savefig(savename)
     if not showinline:
